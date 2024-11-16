e020cada4a729182b66535a50738025db5068dc5
Hakob23 <hakob.arshakyan23@gmail.com>
2024-11-16 06:12:57 +0400

    Initial scripts and necessary remappings

---
+++ test/Vault.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Vault} from "../src/Vault.sol";
+
+contract VaultTest is Test {
+    Vault public vault;
+
+    function setUp() public {
+        vault = new Vault();
+    }
+
+}
